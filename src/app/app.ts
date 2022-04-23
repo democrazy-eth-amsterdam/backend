d22e954cdff5c7743198109f59535666604d9121
Roshan Palakkal <roshan@Roshans-MacBook-Pro.local>
2022-04-23 19:12:46 +0200

    added post

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,11 +2,20 @@ import express = require("express")
 import config = require("../configs/config.json")
 import firebase = require("firebase/firestore")
 import firebaseapp = require("firebase/app")
+import bodyParser = require('body-parser')
 
 const app = firebaseapp.initializeApp(config.firebaseConfig)
 const db = firebase.getFirestore(app)
+const parser = bodyParser.json()
+
+
 
 const server = express()
+server.use(bodyParser.json());
+// server.use(bodyParser.urlencoded());
+// // in latest body-parser use like below.
+server.use(bodyParser.urlencoded({ extended: true }));
+// server.use(express.json())
 
 //#region view
 server.get("/dao/:id", async (req, res) => {
@@ -61,6 +70,23 @@ server.get("/dao/:id/proposals/:proposalId", async (req, res) => {
     )
     proposalInfo = { ...proposalInfo.data(), id: proposalInfo.id }
     return res.status(200).json(proposalInfo)
+})
+
+server.post("/addproposal", parser, async (req, res) => {
+    const proposal = {
+        author: "",
+        daoId: "",
+        description: "",
+        status: {
+            date: new Date(),
+            result: false
+        },
+        title: "",
+        ... req.body
+    }
+    //console.log(req.body)
+    await firebase.addDoc(firebase.collection(db, "proposals"), proposal)
+    return res.status(200).send()
 })
 
 //#endregion
