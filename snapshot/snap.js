28e5d8f01c570d6b1c8932e13c50b8c5662375e0
Roshan Palakkal <roshan@surfer-172-29-3-241-hotspot.internet-for-guests.com>
2022-04-24 06:57:56 +0200

    added ipfs snapshot upload

--- snapshot/snap.js
+++ snapshot/snap.js
@@ -1,4 +1,6 @@
 const fetch = require("node-fetch")
+const IPFS = require("ipfs")
+const converter = require('convert-array-to-csv');
 
 const get_rpc_response = async (method, params = null) => {
     const url = "https://kovan.optimism.io/"
@@ -10,20 +12,26 @@ const get_rpc_response = async (method, params = null) => {
     })
     return await response.json()
 }
-const defaultDict = new Proxy({}, {
-    get: (target, name) => name in target ? target[name] : 0
-})
+const defaultDict = new Proxy(
+    {},
+    {
+        get: (target, name) => (name in target ? target[name] : 0),
+    }
+)
 
 const get_contract_transfers = async (address, toBlock = null) => {
-    const transfer_hash = "0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef"
-    let params = [{"address": address, "fromBlock": '0x0', "topics": [transfer_hash]}]
-    if(toBlock){
+    const transfer_hash =
+        "0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef"
+    let params = [
+        { address: address, fromBlock: "0x0", topics: [transfer_hash] },
+    ]
+    if (toBlock) {
         params[0]["toBlock"] = toBlock
     }
     const logs = await get_rpc_response("eth_getLogs", params)
     const results = logs.result
     const mappedResults = results.map(result => {
-        result.amount = parseInt(result.data, 16) * (10**-18)
+        result.amount = parseInt(result.data, 16) * 10 ** -18
         result.from = result.topics[1].slice(0, 2) + result.topics[1].slice(26)
         result.to = result.topics[2].slice(0, 2) + result.topics[2].slice(26)
         return result
@@ -31,32 +39,35 @@ const get_contract_transfers = async (address, toBlock = null) => {
     return mappedResults
 }
 
-
-
-const get_balances = async (transfers) => {
-    let balances = defaultDict;
-    for(const transfer of transfers){
+const get_balances = async transfers => {
+    let balances = defaultDict
+    for (const transfer of transfers) {
         balances[transfer.from] -= transfer.amount
         balances[transfer.to] += transfer.amount
     }
-    balances = { ...balances}
+    balances = { ...balances }
     balances = Object.keys(balances).map(address => {
         return {
             address,
-            balance: balances[address]
+            balance: balances[address],
         }
     })
     balances = balances.filter(snap => snap.balance > 0.00000000001)
     return balances
 }
 
-module.exports = {
-    takeSnapshot:  async (address, toBlock = null) => {
-        const transfers = await get_contract_transfers(address, toBlock)
-        const balances = await get_balances(transfers)
-        return balances
-    }
+const takeSnapshot = async (address, toBlock = null) => {
+    const transfers = await get_contract_transfers(address, toBlock)
+    const balances = await get_balances(transfers)
+    const ipfs = await IPFS.create()
+    const csvFromArray = converter.convertArrayToCSV(balances)
+    const { cid } = await ipfs.add(csvFromArray)
+    console.log(cid)
+    return {balances, cid}
 }
 
+module.exports = {
+    takeSnapshot,
+}
 
-//console.log(await takeSnapshot("0x86db8fc255d46aa692dbc2050c4a26c1ce61711e"))
+//console.log(takeSnapshot("0x86db8fc255d46aa692dbc2050c4a26c1ce61711e"))
